7ef3b4dbe9cf5334b19dcb251cfe976570856e04
Shubham Mishra <shubham.smishra2004@gmail.com>
2026-06-06 23:36:46 +0530

    added package manager for project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shubham Mishra",
+    author_email="shubham.smishra2004@gmail.com",
+    packages=find_packages()
+)
