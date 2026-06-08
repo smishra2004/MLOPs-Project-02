c3cb42228e493911d3bde0f9c5a75f210d663e9e
Shubham Mishra <shubham.smishra2004@gmail.com>
2026-06-08 22:35:25 +0530

    modified constants

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -7,7 +7,11 @@ COLLECTION_NAME = "Proj2-Data"
 MONGODB_URL_KEY = "MONGODB_URL"
 
 #AWS connection
-BUCKET_NAME = "chest-x-ray-dataset-cnn"
+S3_BUCKET_NAME = "chest-x-ray-dataset-cnn"
 AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
 REGION_NAME = "us-east-1"
+S3_DATA_KEY = "chest-deep-learning.zip"
+
+PIPELINE_NAME: str = ""
+ARTIFACT_DIR: str = "artifacts"
